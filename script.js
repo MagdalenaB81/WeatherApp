42eaf7dc88bba5dc7cf28d1d9e0a41b9b0b77f26
MagdalenaB81 <mbialek81@gmail.com>
2024-10-14 22:11:59 +0200

    updated js with time and date

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  temperatureElement.innerHTML = response.data.temperature.current;
-
-  
   let temperature = response.data.temperature.current;
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  temperatureElement.innerHTML = response.data.temperature.current;
+  
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 
   cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = "${response.data.temperature.humidity}%";
+  windSpeedElement.innerHTML = "${response.data.wind.speed}km/h";
+  timeElement.innerHTML = "${date.getDay()} ${date.getHours()}:${date.getMinutes()}";
 }
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+} 
 
 function searchCity(city) {
   let apiKey = "9a5aabc4e027cb0ac43o63ce8c4fb0t3";
@@ -31,4 +59,5 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 
-searchCity("Barcelona");
+
+
